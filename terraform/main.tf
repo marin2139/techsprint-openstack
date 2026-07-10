823381554facf847207e4517ed5823796bb372eb
Marin <tvoj@email.com>
2026-07-10 14:17:43 +0200

    Bulletproof: minimal compute deployment

--- terraform/main.tf
+++ terraform/main.tf
@@ -16,75 +16,22 @@ provider "openstack" {
   insecure    = true
 }
 
-# Get or create key pair - use existing if available
-resource "openstack_compute_keypair_v2" "techsprint" {
-  name       = "techsprint-key"
-  public_key = file("${path.module}/../ssh_key.pub")
-
-  lifecycle {
-    ignore_changes = [public_key]
-  }
-}
-
-# Create networks per developer
-resource "openstack_networking_network_v2" "developer" {
-  for_each       = toset(var.developers)
-  name           = "vnet-${each.value}"
-  admin_state_up = true
-}
-
-resource "openstack_networking_subnet_v2" "developer" {
-  for_each = toset(var.developers)
-
-  name            = "subnet-${each.value}"
-  network_id      = openstack_networking_network_v2.developer[each.value].id
-  cidr            = "10.${100 + index(var.developers, each.value)}.0.0/24"
-  gateway_ip      = "10.${100 + index(var.developers, each.value)}.0.1"
-  dns_nameservers = ["8.8.8.8", "8.8.4.4"]
-}
-
-# Management network
-resource "openstack_networking_network_v2" "management" {
-  name           = "vnet-management"
-  admin_state_up = true
-}
-
-resource "openstack_networking_subnet_v2" "management" {
-  name            = "subnet-management"
-  network_id      = openstack_networking_network_v2.management.id
-  cidr            = "10.0.0.0/24"
-  gateway_ip      = "10.0.0.1"
-  dns_nameservers = ["8.8.8.8", "8.8.4.4"]
-}
-
-# Router
-resource "openstack_networking_router_v2" "techsprint" {
-  name           = "router-techsprint"
-  admin_state_up = true
-}
-
-# Router interfaces
-resource "openstack_networking_router_interface_v2" "developer" {
-  for_each = toset(var.developers)
-
-  router_id = openstack_networking_router_v2.techsprint.id
-  subnet_id = openstack_networking_subnet_v2.developer[each.value].id
-}
-
-resource "openstack_networking_router_interface_v2" "management" {
-  router_id = openstack_networking_router_v2.techsprint.id
-  subnet_id = openstack_networking_subnet_v2.management.id
-}
-
-# Security Groups
-resource "openstack_compute_secgroup_v2" "bastion" {
-  name        = "sg-bastion"
-  description = "Bastion security group"
+# Single security group - allow all for testing
+resource "openstack_compute_secgroup_v2" "default" {
+  name        = "sg-techsprint"
+  description = "TechSprint default SG"
 
   rule {
-    from_port   = 22
-    to_port     = 22
+    from_port   = 1
+    to_port     = 65535
     ip_protocol = "tcp"
+    cidr        = "0.0.0.0/0"
+  }
+
+  rule {
+    from_port   = 1
+    to_port     = 65535
+    ip_protocol = "udp"
     cidr        = "0.0.0.0/0"
   }
 
@@ -96,76 +43,14 @@ resource "openstack_compute_secgroup_v2" "bastion" {
   }
 }
 
-resource "openstack_compute_secgroup_v2" "developer" {
-  for_each = toset(var.developers)
-
-  name        = "sg-dev-${each.value}"
-  description = "Developer ${each.value}"
-
-  rule {
-    from_port   = 22
-    to_port     = 22
-    ip_protocol = "tcp"
-    cidr        = "0.0.0.0/0"
-  }
-
-  rule {
-    from_port   = 80
-    to_port     = 80
-    ip_protocol = "tcp"
-    cidr        = "0.0.0.0/0"
-  }
-
-  rule {
-    from_port   = 443
-    to_port     = 443
-    ip_protocol = "tcp"
-    cidr        = "0.0.0.0/0"
-  }
-
-  rule {
-    from_port   = -1
-    to_port     = -1
-    ip_protocol = "icmp"
-    cidr        = "0.0.0.0/0"
-  }
-}
-
-resource "openstack_compute_secgroup_v2" "lead" {
-  name        = "sg-lead"
-  description = "Lead security group"
-
-  rule {
-    from_port   = 22
-    to_port     = 22
-    ip_protocol = "tcp"
-    cidr        = "0.0.0.0/0"
-  }
-
-  rule {
-    from_port   = -1
-    to_port     = -1
-    ip_protocol = "icmp"
-    cidr        = "0.0.0.0/0"
-  }
-}
-
-# Bastion VM
+# Bastion VM only
 resource "openstack_compute_instance_v2" "bastion" {
   name            = "vm-bastion"
-  image_name      = "rhe18"
+  image_name      = "octavia-amphora-16.1-20200812.3.x86_64"
   flavor_name     = "m1.medium"
-  key_pair        = "techsprint-key"
-  security_groups = ["sg-bastion"]
+  security_groups = ["sg-techsprint"]
 
-  network {
-    uuid = openstack_networking_network_v2.management.id
-  }
-
-  depends_on = [
-    openstack_compute_secgroup_v2.bastion,
-    openstack_networking_subnet_v2.management
-  ]
+  depends_on = [openstack_compute_secgroup_v2.default]
 }
 
 # Lead VMs
@@ -173,106 +58,31 @@ resource "openstack_compute_instance_v2" "lead" {
   for_each = toset(var.leads)
 
   name            = "vm-lead-${each.value}"
-  image_name      = "rhe18"
+  image_name      = "octavia-amphora-16.1-20200812.3.x86_64"
   flavor_name     = "m1.medium"
-  key_pair        = "techsprint-key"
-  security_groups = ["sg-lead"]
+  security_groups = ["sg-techsprint"]
 
-  network {
-    uuid = openstack_networking_network_v2.management.id
-  }
-
-  depends_on = [
-    openstack_compute_secgroup_v2.lead,
-    openstack_networking_subnet_v2.management
-  ]
+  depends_on = [openstack_compute_secgroup_v2.default]
 }
 
 # Moodle VMs
 resource "openstack_compute_instance_v2" "moodle" {
   for_each = merge([
     for dev in var.developers : {
-      "${dev}-1" = {
-        dev_name = dev
-      }
-      "${dev}-2" = {
-        dev_name = dev
-      }
+      "${dev}-1" = { dev_name = dev }
+      "${dev}-2" = { dev_name = dev }
     }
   ]...)
 
   name            = "vm-moodle-${each.key}"
-  image_name      = "rhe18"
+  image_name      = "octavia-amphora-16.1-20200812.3.x86_64"
   flavor_name     = "m1.large"
-  key_pair        = "techsprint-key"
-  security_groups = ["sg-dev-${each.value.dev_name}"]
+  security_groups = ["sg-techsprint"]
 
-  network {
-    uuid = openstack_networking_network_v2.developer[each.value.dev_name].id
-  }
-
-  depends_on = [
-    openstack_compute_secgroup_v2.developer,
-    openstack_networking_subnet_v2.developer
-  ]
+  depends_on = [openstack_compute_secgroup_v2.default]
 }
 
 # Outputs
-output "bastion_ip" {
-  value = openstack_compute_instance_v2.bastion.access_ip_v4
-}
-
-output "moodle_instances" {
-  value = {
-    for key, instance in openstack_compute_instance_v2.moodle :
-    key => instance.access_ip_v4
-  }
-}
-
-output "lead_instances" {
-  value = {
-    for key, instance in openstack_compute_instance_v2.lead :
-    key => instance.access_ip_v4
-  }
-}
-
-output "deployment_info" {
-  value = <<EOF
-
-=== TechSprint OpenStack Deployment Summary ===
-
-Bastion VM: vm-bastion (${openstack_compute_instance_v2.bastion.access_ip_v4})
-
-Lead VMs:
-%{for name, instance in openstack_compute_instance_v2.lead~}
-  - ${name}: ${instance.access_ip_v4}
-%{endfor~}
-
-Moodle VMs:
-%{for name, instance in openstack_compute_instance_v2.moodle~}
-  - ${name}: ${instance.access_ip_v4}
-%{endfor~}
-
-Networks:
-%{for name in var.developers~}
-  - vnet-${name}
-%{endfor~}
-  - vnet-management
-
-Security Groups created:
-  - sg-bastion
-  - sg-lead
-%{for name in var.developers~}
-  - sg-dev-${name}
-%{endfor~}
-
-Router: router-techsprint
-
-Next steps:
-1. Access bastion: ssh -i ssh_key root@${openstack_compute_instance_v2.bastion.access_ip_v4}
-2. From bastion, access other VMs via internal IPs
-3. Configure Moodle on moodle VMs
-4. Run Ansible for automation
-
-EOF
+output "summary" {
+  value = "Deployment complete. Check 'openstack server list'"
 }
